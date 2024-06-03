2689916927d9e13e4f118d0d39c302ef762b73b1
kelemenkf <83816808+kelemenkf@users.noreply.github.com>
2024-06-03 16:01:10 +0200

    Add demonstrations.py

---
+++ demonstrations.py
@@ -0,0 +1,17 @@
+from repos.multifractal.simulator import Simulator
+
+
+def plot_scaling(n=3, process='bm', H=0.5):
+    for t in [2**i for i in range(1, n+1)]:
+        sim = Simulator(sim_type=process, T=1000*t, dt_scale=t, H=H)
+        sim.plot_process()
+
+
+
+def mean_squared_distance_relationship(n=3, process='bm', H=0.5):
+    MSDS = []
+    for t in  [2**i for i in range(1, n+1)]:
+        sim = Simulator(process, T=1000*t, dt_scale=t, H=H)
+        msd = sim.msd(10000)
+        MSDS.append(msd)
+    return MSDS
